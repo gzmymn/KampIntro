b2735c266f067aaf72cacd455370a2f58683dd8b
Gizem Kübra Yaman <gizem.2406@gmail.com>
2021-02-21 21:46:13 +0300

    Ref Out ödevi

---
+++ RefOut/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RefOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 5;
+            int sayi2 = 10;
+            int sayi3;
+            int sayi4 = 15;
+
+            Topla(ref sayi1, sayi2);
+            
+            Carp(out sayi3, sayi4);
+
+        }
+        public static void Topla(ref int sayi1, int sayi2)
+        {
+            sayi1 = 30;
+            var sonuc1 = sayi1 + sayi2;
+            
+            Console.WriteLine("Toplam=" + sonuc1);
+        }  
+        
+        public static void Carp(out int sayi3, int sayi4)
+        {
+            sayi3 = 20;
+            var sonuc2 = sayi3 * sayi4;
+
+            Console.WriteLine("Çarpım=" + sonuc2);
+        }
+    }
+}
